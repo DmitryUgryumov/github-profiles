7c880b634957d5881131c693fc87448e376f328d
Dmitry <yonkers228@gmail.com>
2021-12-13 13:54:28 +0500

    change home page

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
       <Header />
 
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/github-profiles">
           <Search />
         </Route>
         <Route path="/repos/:login">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-container">
-        <Link className="header__title" to="/">
+        <Link className="header__title" to="/github-profiles">
           GitHub profiles
         </Link>
         <div className="header__button-container">
